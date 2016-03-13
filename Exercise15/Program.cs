ae0574f0ba9bdf1cab072ccc248f1d721b19fa82
Dalius Pamparas <dalius.pamparas@gmail.com>
2016-03-13 20:56:16 +0100

    Exercise 15

---
+++ Exercise15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            string sentence = "Hello world and some more words";
+
+
+             
+
+        }
+    }
+}
