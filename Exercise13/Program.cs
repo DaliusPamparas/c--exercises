168157fb505e4818a002dd8643ca9a686cb9e067
Dalius Pamparas <dalius.pamparas@gmail.com>
2016-03-16 21:11:57 +0100

    exercise 13, need find bug line 30-46

--- Exercise13/Program.cs
+++ Exercise13/Program.cs
@@ -26,18 +26,28 @@ namespace Exercise13
             TimeSpan currentAge = new TimeSpan();
             currentAge = DateTime.Now - birthdate;
             int myAge = currentAge.Days / 365;
-           
 
-            TimeSpan daysToBirthday = new TimeSpan();
-            
-            DateTime nexBirthDate = new DateTime(, month, day);
+//
+            int daysToBirtday = 0;
+            DateTime fromToday = DateTime.Now;
+            int thisYear = fromToday.Year;
+            DateTime nexBirthDate = new DateTime(thisYear, month, day);
 
+            if (nexBirthDate < fromToday)
+            {
+                nexBirthDate.AddYears(1);
+            }
 
+            while (fromToday < nexBirthDate)
+            {
+                daysToBirtday = daysToBirtday + 1;
+                fromToday.AddDays(1);
+            }
 
-
+//
             Console.WriteLine("Hello, " + name + " .\n" + "You are " 
                 + myAge + " years old and will turn age " + (myAge + 1) + " in"+
-               daysToBirthday + "days");
+               daysToBirtday + "days");
             Console.ReadKey();
           
 
