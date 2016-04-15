1cc0fb6a74740ddc9dd4a02415efc3020618ee99
Dalius Pamparas <dalius.pamparas@gmail.com>
2016-04-15 10:17:13 +0200

    10

---
+++ Exercise10/Bicykle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10
+{
+    class Bicycle : Vehicle
+    {
+        public bool dinamoON { get; set; } 
+        public Bicycle() : base("bicycle")
+        {
+
+        }
+        public void DinamoON()
+        {
+            dinamoON = true;
+        }
+        public void Dinamoff()
+        {
+            dinamoON = false;
+        }
+    }
+
+}
