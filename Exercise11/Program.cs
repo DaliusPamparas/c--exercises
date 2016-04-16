7b3dab017961bd5c09a88418b9c6a573efa1420d
Dalius Pamparas <dalius.pamparas@gmail.com>
2016-04-16 18:46:26 +0200

    11 och 12

---
+++ Exercise11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VehicleData data = new VehicleData();
+
+            Car mustang = new Car();
+
+            data.PrintVehicle(mustang);
+
+            Bicycle trehjul = new Bicycle();
+
+            data.PrintVehicle(trehjul);
+            Console.ReadKey();
+
+        }
+    }
+}
